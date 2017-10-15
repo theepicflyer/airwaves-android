12afdd79538cb7a01847f69abe1150c807e8e96c
theepicflyer <xiejc2000@gmail.com>
2017-10-15 09:35:10 +0800

    Changed join group slightly, prepare for second activity

--- app/src/main/java/io/makerforce/airwaves/GroupActivity.java
+++ app/src/main/java/io/makerforce/airwaves/GroupActivity.java
@@ -3,23 +3,18 @@ package io.makerforce.airwaves;
 
 import android.media.MediaPlayer;
 import android.os.Bundle;
-import android.os.CountDownTimer;
 import android.os.Handler;
-import android.os.StrictMode;
 import android.os.SystemClock;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-import java.io.IOException;
-import java.net.URI;
 import java.net.URISyntaxException;
 
 import io.socket.client.IO;
@@ -27,11 +22,11 @@ import io.socket.client.Socket;
 import io.socket.emitter.Emitter;
 
 
-public class MainActivity extends AppCompatActivity {
+public class GroupActivity extends AppCompatActivity {
     String ip = "http://172.22.152.16:10202";
     long diff;
     final int delay = 1000;
-    private Socket mSocket;
+    public Socket mSocket;
     {
         try {
             mSocket = IO.socket(ip);
@@ -47,7 +42,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -59,23 +54,19 @@ public class MainActivity extends AppCompatActivity {
 //        }
 
         mediaPlayer = MediaPlayer.create(this, R.raw.test);
+        final Button joinButton = (Button) findViewById(R.id.joinButton);
+        final EditText groupIDText = (EditText) findViewById(R.id.groupIDText);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                mSocket.emit("joingroup", groupIDText.getText());
+                // TODO: Start a second activity
             }
         });
 
-        final Button joinButton = (Button) findViewById(R.id.joinButton);
-        final EditText groupIDText = (EditText) findViewById(R.id.groupIDText);
-        joinButton.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v) {
-                mSocket.emit("joingroup", groupIDText.getText());
-            }
-        });
+
         Log.d(TAG, "Before Connect");
          //Move this where needed
         mSocket.on("timepong", new Emitter.Listener() {
@@ -109,6 +100,7 @@ public class MainActivity extends AppCompatActivity {
 
         mSocket.connect();
     }
+
 
     public Emitter.Listener onPong = new Emitter.Listener() {
         @Override
